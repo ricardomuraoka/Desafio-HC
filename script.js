27b8fe0d76d4dcca3aceb0143bbcb07a51206bc7
Ricardo <ricardomr8@hotmail.com>
2021-07-16 08:09:10 -0300

    Atualização
    Ultima atualização

--- script.js
+++ script.js
@@ -6,8 +6,5 @@ function gravar(){
     var increment = localStorage.length+1
     localStorage.setItem(increment,email)
     mail.value = null
-    let heading = document.getElementById('heading')
-    let sub = `<p> Subscribing </p>`
-    heading.innerHTML = sub
-}
+    }
 
